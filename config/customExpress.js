a4924258d8645685b8ff494603c35b4715d0d0a5
Rodrigo Lorandi <rodrigo_lorandi@hotmail.com>
2021-07-16 11:42:10 -0300

    add app.post

--- config/customExpress.js
+++ config/customExpress.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const consign = require('consign');
+const bodyParser = require('body-parser');
 
 module.exports = () =>{
   const app = express();
+  app.use(express.json())
+  app.use(express.urlencoded({ extended: true}))
 
 consign()
   .include('controllers')
