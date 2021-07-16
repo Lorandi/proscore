7b6724042a9047cd1eaf5d879600141b7c99510e
Rodrigo Lorandi <rodrigo_lorandi@hotmail.com>
2021-07-16 12:16:54 -0300

    connection.connect()

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 const customExpress = require('./config/customExpress');
+const connection = require('./database/connection');
 
-const app = customExpress();
+connection.connect(error =>{
+  if(error){
+    console.log(error)
+  }else{
+    console.log("database connection established  porta 3306")
+    const app = customExpress();
+    app.listen(3000, ()=> console.log("server at port 3000"))
+  }
+})
 
-app.listen(3000, ()=> console.log("rodando terminal"))
+
 
