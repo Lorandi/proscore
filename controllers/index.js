908392ab965fb048f452e7533317533123d90ec4
Rodrigo Lorandi <rodrigo_lorandi@hotmail.com>
2021-07-16 14:49:52 -0300

    create folder models

--- controllers/index.js
+++
@@ -1,9 +0,0 @@
-
-module.exports = app => {
-  app.get('/', (req, res) => res.send('realizando GET'))
-
-  app.post('/', (req, res) => {
-    console.log(req.body)
-    res.send('realizando POST')
-  })
-}
--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const customExpress = require('./config/customExpress');
 const connection = require('./database/connection');
+const Tables = require('./database/tables.js')
 
 connection.connect(error =>{
   if(error){
     console.log(error)
   }else{
-    console.log("database connection established  porta 3306")
+    console.log("database connection established  port 3306")
+    Tables.init(connection)
     const app = customExpress();
     app.listen(3000, ()=> console.log("server at port 3000"))
   }
