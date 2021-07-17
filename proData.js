f0088d11da9780de910cfdeec0aa5f1e797075d5
Rodrigo Lorandi <rodrigo_lorandi@hotmail.com>
2021-07-17 20:40:08 -0300

    fake data

---
+++ proData.js
@@ -0,0 +1,14 @@
+var proData = {
+  "age": 20,
+  "education_level": "bachelors_degree_or_high",
+  "past_experiences": {
+    "sales": false, 
+    "support": false, },
+  "internet_test": { 
+    "download_speed": 51, 
+    "upload_speed": 51  },
+  "writing_score": 0.8,
+  "referral_code": "token1234"
+}
+
+module.exports = proData;
