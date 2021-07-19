59b9381cd528a0e6d872bae7022502f5a471fb29
Rodrigo Lorandi <rodrigo_lorandi@hotmail.com>
2021-07-19 12:45:47 -0300

    Update proResult

--- proResult.js
+++ proResult.js
@@ -4,9 +4,12 @@ const proProject = require('./proProject')
 const proResult = (props) => {  
   const score = updateScore(props);
   const project = proProject(props);
+  
   const proFinalResult = {
     "score": score, 
-    "selected_project": project,
+    "selected_project": project.selected_project,
+    "eligible_projects":  project.eligible_projects,
+    "ineligible_projects": project.ineligible_projects,
  }
   return proFinalResult;
 }
