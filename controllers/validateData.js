e237c032011da288f8e508ef47f032ee0d833d68
Rodrigo Lorandi <rodrigo_lorandi@hotmail.com>
2021-07-20 11:30:36 -0300

    create validateData.js

---
+++ controllers/validateData.js
@@ -0,0 +1,14 @@
+const validateData = (pro) => {
+  const educationLevel = pro.education_level;
+  const sales = pro.past_experiences.sales;
+  const support = pro.past_experiences.support;
+  const downloadSpeed = pro.internet_test.download_speed;
+  const uploadSpeed = pro.internet_test.upload_speed;
+  const writingScore = pro.writing_score;
+  const referralSode = pro.referral_code;
+
+  let invalid = "";
+
+  return invalid;
+}
+module.exports = validateData;
