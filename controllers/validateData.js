fea0efe3b568bbd8c0ac9da7d83266244dc5d512
Rodrigo Lorandi <rodrigo_lorandi@hotmail.com>
2021-07-20 15:20:36 -0300

    update eslint

--- controllers/validateData.js
+++ controllers/validateData.js
@@ -1,4 +1,5 @@
 const validateData = (pro) => {
+  const age = pro.age
   const educationLevel = pro.education_level
   const sales = pro.past_experiences.sales
   const support = pro.past_experiences.support
@@ -8,32 +9,40 @@ const validateData = (pro) => {
 
   let invalid = ''
 
+  if (typeof age !== 'number' || !Number.isInteger(age) || age < 0) {
+    console.log('Invalid age: Attributes aren´t a integer equal or greater than 0 ')
+    invalid += 'Invalid age: Attributes aren´t a integer equal or greater than 0 .'
+  }
+
   if (
     educationLevel !== 'no_education' &&
     educationLevel !== 'high_school' &&
     educationLevel !== 'bachelors_degree_or_high') {
-    console.log('Invalid Education Level')
-    invalid += 'Invalid Education Level. '
+    console.log('Invalid Education Level. Must be "no_education", "high_school" or "bachelors_degree_or_high")')
+    invalid += 'Invalid Education Level. Must be no_education, high_school or bachelors_degree_or_high '
   }
 
   if (typeof sales !== 'boolean') {
-    console.log('Invalid past experiences sales. Parameters must be boolean')
-    invalid += 'Invalid past experiences sales. Parameters must be boolean. '
+    console.log('Invalid past experiences sales. Attributes aren´t boolean')
+    invalid += 'Invalid past experiences sales. Attributes aren´t boolean. '
   }
 
   if (typeof support !== 'boolean') {
-    console.log('Invalid past experiences support. Parameters must be boolean')
-    invalid += 'Invalid past experiences support. Parameters must be boolean. '
+    console.log('Invalid past experiences support. Attributes aren´t boolean')
+    invalid += 'Invalid past experiences support. Attributes aren´t boolean. '
   }
 
-  if (typeof downloadSpeed !== 'number' || typeof uploadSpeed !== 'number') {
-    console.log('Invalid internet test. Parameters must be a number')
-    invalid += 'Invalid internet test. Parameters must be a number. '
+  if (typeof downloadSpeed !== 'number' ||
+   typeof uploadSpeed !== 'number' ||
+   downloadSpeed < 0 ||
+   uploadSpeed < 0) {
+    console.log('Invalid internet test. Attributes aren´t positive floats')
+    invalid += 'Invalid internet test. Attributes aren´t positive floats. '
   }
 
   if (typeof writingScore !== 'number' || (writingScore < 0 || writingScore > 1)) {
-    console.log('Invalid writing score. Parameters must be a float between 0 and 1 ')
-    invalid += 'Invalid writing score. Parameters must be a float between 0 and 1. '
+    console.log('Invalid writing score. Attributes aren´t float between 0 and 1 ')
+    invalid += 'Invalid writing score. Attributes aren´t float between 0 and 1. '
   }
 
   return invalid
