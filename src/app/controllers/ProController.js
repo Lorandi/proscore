d97b72e0dd0b98283c8a637578130cf0c2d28a92
Rodrigo Lorandi <rodrigo_lorandi@hotmail.com>
2021-08-05 16:12:54 -0300

    modify ProController.js

--- src/app/controllers/ProController.js
+++ src/app/controllers/ProController.js
@@ -13,31 +13,41 @@ class ProController {
   }
   addPro(event) {
     event.preventDefault();
-   
-    let pro = {
-      age: this._age.value,
-      education_level: this._education_level.value,
-      past_experiences: {
-        sales: this._sales.checked,
-        support: this._support.checked,
-      },
-      internet_test: {
-        download_speed: this._download_speed.value,
-        upload_speed: this._upload_speed.value
-      },
-      writing_score: this._writing_score.value,
-      referral_code: this._referral_code.value
-    };
+
+    let pro = new Pro(
+      this._age.value,
+      this._education_level.value,
+      this._sales.checked,
+      this._support.checked,
+      this._download_speed.value,
+      this._upload_speed.value,
+      this._writing_score.value,
+      this._referral_code.value
+    );
+
+    // let pro = {
+    //   age: this._age.value,
+    //   education_level: this._education_level.value,
+    //   past_experiences: {
+    //     sales: this._sales.checked,
+    //     support: this._support.checked,
+    //   },
+    //   internet_test: {
+    //     download_speed: this._download_speed.value,
+    //     upload_speed: this._upload_speed.value
+    //   },
+    //   writing_score: this._writing_score.value,
+    //   referral_code: this._referral_code.value
+    // };
 
     console.log(pro);
 
     let pre = document.querySelector("#msg pre");
     pre.textContent = "\n" + JSON.stringify(pro, "\t", 2);
 
+    console.log("\n" + JSON.stringify(pro, "\t", 2));
     document.querySelector('form').reset()
 
-    console.log("\n" + JSON.stringify(pro, "\t", 2));
-    
   }
 }
 
