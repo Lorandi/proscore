c3d683b0d9ced6d3c5567531a8927ac9e085dcc2
Rodrigo Lorandi <rodrigo_lorandi@hotmail.com>
2021-08-05 08:10:57 -0300

    add _ to variables on ProController

--- src/app/controllers/ProController.js
+++ src/app/controllers/ProController.js
@@ -2,31 +2,31 @@ class ProController {
 
   constructor() {
     let $ = document.querySelector.bind(document);
-    this.age = $('#age');
-    this.education_level = ($("#educationLevel"));
-    this.sales = ($("#sales"));
-    this.support = ($("#support"));
-    this.download_speed = ($("#download_speed"));
-    this.upload_speed = ($("#upload_speed"));
-    this.writing_score = ($("#writing_score"));
-    this.referral_code = ($("#referral_code"));
+    this._age = $('#age');
+    this._education_level = ($("#educationLevel"));
+    this._sales = ($("#sales"));
+    this._support = ($("#support"));
+    this._download_speed = ($("#download_speed"));
+    this._upload_speed = ($("#upload_speed"));
+    this._writing_score = ($("#writing_score"));
+    this._referral_code = ($("#referral_code"));
   }
   addPro(event) {
     event.preventDefault();
    
     let pro = {
-      age: this.age.value,
-      education_level: this.education_level.value,
+      age: this._age.value,
+      education_level: this._education_level.value,
       past_experiences: {
-        sales: this.sales.checked,
-        support: this.support.checked,
+        sales: this._sales.checked,
+        support: this._support.checked,
       },
       internet_test: {
-        download_speed: this.download_speed.value,
-        upload_speed: this.upload_speed.value
+        download_speed: this._download_speed.value,
+        upload_speed: this._upload_speed.value
       },
-      writing_score: this.writing_score.value,
-      referral_code: this.referral_code.value
+      writing_score: this._writing_score.value,
+      referral_code: this._referral_code.value
     };
 
     console.log(pro);
